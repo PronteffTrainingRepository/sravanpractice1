56dd1703eb460ffe7a2ad1975c4edebc3e0ae519
sravanpronteff <67730088+sravanpronteff@users.noreply.github.com>
2020-07-04 17:28:20 +0530

    04-07-2020
    worked on introduction

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
 import React from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Hi from './View/Hi';
+import Welcome from './View/Welcome';
+
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Hello 
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      
+      <Welcome/>
+      <Hi/>
+
+
     </div>
   );
 }
---
+++ src/View/Hi.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from '../logo.svg';
+import '../../src/App.css';
+
+
+
+function App() {
+        return (
+                <div className="App">
+
+                        <p>Hello This is react tutorail</p>
+
+
+                </div>
+        );
+}
+
+export default App;
